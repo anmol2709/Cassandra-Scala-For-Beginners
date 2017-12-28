0f1a0212610c0653310b1f4d720bd84fa21b2358
anmol <anmol.sarna@knoldus.in>
2017-12-28 17:48:25 +0530

    adding update queries

--- src/main/scala/CassandraScalaDemo.scala
+++ src/main/scala/CassandraScalaDemo.scala
@@ -1,4 +1,4 @@
-import com.datastax.driver.core.Cluster
+import com.datastax.driver.core._
 import org.apache.log4j.Logger
 
 object CassandraScalaDemo extends App {
@@ -6,10 +6,14 @@ object CassandraScalaDemo extends App {
   val log = Logger.getLogger(this.getClass)
 
   //creating Cluster object
-  val cluster = Cluster.builder().addContactPoint("localhost").build()
+  val cluster = Cluster.builder().addContactPoint("localhost").withQueryOptions(new QueryOptions().setConsistencyLevel(ConsistencyLevel.ONE)).build()
 
   //Creating Session object
   val session = cluster.connect()
+
+Thread.sleep(2000)
+  val consistencylevel = cluster.getConfiguration.getQueryOptions.getConsistencyLevel()
+    log.info("Current consistency level is  " + consistencylevel )
 
   //Executing the query for creating KeySpace
   val createKeySpaceQuery = "CREATE KEYSPACE cassandraDemo WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};"
@@ -23,7 +27,9 @@ object CassandraScalaDemo extends App {
   Thread.sleep(2000)
 
   //Creating the Table
-  val createTableQuery = "CREATE TABLE employee(emp_id int, emp_name text,emp_city text,emp_salary varint,emp_phone varint,PRIMARY KEY(emp_id,emp_salary));"
+  val createTableQuery = "CREATE TABLE employee" +
+    "(emp_id int, emp_name text,emp_city text,emp_salary varint" +
+    ",emp_phone varint,PRIMARY KEY(emp_id,emp_salary));"
   session.execute(createTableQuery)
   log.info("Table created")
 
@@ -31,11 +37,10 @@ object CassandraScalaDemo extends App {
   //Just waiting for creating the tables to perform insert operation
 
   // Insert Queries
-  val insertQuery1 = "INSERT INTO employee(emp_id, emp_name,emp_city ,emp_salary ,emp_phone) VALUES(1,'Name1','Delhi',50000,9876543210)"
-  val insertQuery2 = "INSERT INTO employee(emp_id, emp_name,emp_city ,emp_salary ,emp_phone) VALUES(2,'Name2','Noida',70000,987654211)"
-  val insertQuery3 = "INSERT INTO employee(emp_id, emp_name,emp_city ,emp_salary ,emp_phone) VALUES(3,'Name3','Faridabad',20000,9876543212)"
-  val insertQuery4 = "INSERT INTO employee(emp_id, emp_name,emp_city ,emp_salary ,emp_phone) VALUES(4,'Name4','Gurgaon',80000,9876543213)"
-
+  val insertQuery1 = "INSERT INTO employee(emp_id, emp_name,emp_city ,emp_salary ,emp_phone) VALUES(1,'Name1','Delhi',50000,9876543210) IF NOT EXISTS"
+  val insertQuery2 = "INSERT INTO employee(emp_id, emp_name,emp_city ,emp_salary ,emp_phone) VALUES(2,'Name2','Noida',70000,987654211) IF NOT EXISTS"
+  val insertQuery3 = "INSERT INTO employee(emp_id, emp_name,emp_city ,emp_salary ,emp_phone) VALUES(3,'Name3','Faridabad',20000,9876543212) IF NOT EXISTS"
+  val insertQuery4 = "INSERT INTO employee(emp_id, emp_name,emp_city ,emp_salary ,emp_phone) VALUES(4,'Name4','Gurgaon',80000,9876543213) IF NOT EXISTS"
   session.execute(insertQuery1)
   session.execute(insertQuery2)
   session.execute(insertQuery3)
@@ -50,7 +55,7 @@ object CassandraScalaDemo extends App {
 
   //  SELECT QUERIES
 
-  //Basic Select Query For All Rows
+  //Basic Select Query For   Rows
   val selectQuery1 = "Select * from employee"
   val res = session.execute(selectQuery1)
   log.info(":::  Select all Query :::")
@@ -70,10 +75,23 @@ object CassandraScalaDemo extends App {
   res3.forEach(log.info(_))
 
   //Select query using Partitioning key and Index
-  val selectQuery4 = "Select * from employee where emp_id=1 and emp_name='Name1'"
+  val selectQuery4 = "Select * from employee where emp_name='Name1'"
   val res4 = session.execute(selectQuery4)
   log.info(":::  Select all with Emp_id = 1  using index name::: ")
   res4.forEach(log.info(_))
+
+  val updateQuery = "UPDATE employee SET emp_name = 'Name5' WHERE emp_id =1 AND emp_salary =50000"
+  session.execute(updateQuery)
+  log.info("Row Updated")
+
+  Thread.sleep(2000)
+
+  val updateQuery2 = "UPDATE employee SET emp_name = 'Name6' WHERE emp_id =6 AND emp_salary =50000"
+  session.execute(updateQuery2)
+  log.info("Row Updated/Added")
+  Thread.sleep(2000)
+
+//COMMENT THESE IF YOU NEED TO KEEP THE DATA IN CASSANDRA 
 
   //Removing the Data From the Table
   val truncateQuery = "Truncate table employee"
@@ -84,6 +102,10 @@ object CassandraScalaDemo extends App {
   val dropTableQuery = "DROP TABLE employee"
   session.execute(dropTableQuery)
   log.info("Table dropped")
+
+ val dropKeyspace = "DROP KEYSPACE cassandrademo"
+  session.execute(dropKeyspace)
+  log.info("Keyspace dropped")
 
   //Closing the cluster
   cluster.close()
